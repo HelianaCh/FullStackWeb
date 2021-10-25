5bfc250b21f7bf0e74ac38c4a0529782ace0dd95
heliana <helianachristianti@gmail.com>
2021-10-25 17:19:26 +0700

    database and migration
    sudah dapat membuat database laragon dan sudah di migrate juga tabel2 nya

--- bwastore-laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ bwastore-laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,25 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create("users", function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string("name");
+            $table->string("email")->unique();
+            $table->timestamp("email_verified_at")->nullable();
+            $table->string("password");
+
+            $table->longText("address_one");
+            $table->longText("address_two");
+            $table->integer("provinces_id");
+            $table->integer("regencies_id");
+            $table->integer("zip_code");
+            $table->string("country");
+            $table->string("phone_number");
+            $table->string("store_name");
+            $table->integer("categories_id");
+            $table->integer("store_status");
+
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,6 +44,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists("users");
     }
 }
