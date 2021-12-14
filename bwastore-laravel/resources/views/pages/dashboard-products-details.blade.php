9de41595a890fb7d01dffe20cdfd6054dc82c858
heliana <helianachristianti@gmail.com>
2021-12-14 17:36:58 +0700

    Integrasi Dashboard My Product
    Sudah bisa upload + delete produk di dashboard

--- bwastore-laravel/resources/views/pages/dashboard-products-details.blade.php
+++ bwastore-laravel/resources/views/pages/dashboard-products-details.blade.php
@@ -19,7 +19,20 @@
               <div class="dashboard-content">
                 <div class="row">
                   <div class="col-12">
-                    <form action="">
+                    @if($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>
+                                        {{ $error }}
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <form action="{{ route('dashboard-product-update', $product->id) }}" method="POST" enctype="multipart/form-data">
+                      @csrf
+                      <input type="hidden" name="users_id" value="{{ Auth::user()->id }}">
                       <div class="card">
                         <div class="card-body">
                           <div class="row">
@@ -28,8 +41,10 @@
                                 <label>Product Name</label>
                                 <input
                                   type="text"
+                                  name="name"
                                   class="form-control"
                                   value="Papel La Casa"
+                                  value="{{ $product->name }}"
                                 />
                               </div>
                             </div>
@@ -39,8 +54,10 @@
                                 <label>Price</label>
                                 <input
                                   type="number"
+                                  name="price"
                                   class="form-control"
-                                  value="100.00"
+                                  value="200"
+                                  value="{{ $product->price }}"
                                 />
                               </div>
                             </div>
@@ -48,10 +65,11 @@
                             <div class="col-md-12">
                               <div class="form-group">
                                 <label>Kategori</label>
-                                <select name="category" class="form-control">
-                                  <option value="" disabled>
-                                    Select Category
-                                  </option>
+                                <select name="categories_id" class="form-control">
+                                  <option value="{{ $product->categories_id }}">Tidak Diganti ({{ $product->category->name }})</option>
+                                  @foreach ($categories as $categories)
+                                    <option value="{{ $categories->id }}">{{ $categories->name }}</option>
+                                  @endforeach
                                 </select>
                               </div>
                             </div>
@@ -59,7 +77,7 @@
                             <div class="col-md-12">
                               <div class="form-group">
                                 <label>Description</label>
-                                <textarea name="editor"></textarea>
+                                <textarea name="description" id="editor">{!! $product->description !!}</textarea>
                               </div>
                             </div>
 
@@ -82,58 +100,40 @@
                   <div class="col-12">
                     <div class="card-body">
                       <div class="row">
-                        <div class="col-md-4">
-                          <div class="gallery-container">
-                            <img
-                              src="/images/product-card-1.png"
-                              alt=""
-                              class="w-100"
-                            />
-                            <a href="#" class="delete-gallery">
-                              <img src="/images/icon-delete.svg" alt="" />
-                            </a>
+                        @foreach ($product->galleries as $gallery)
+                          <div class="col-md-4">
+                            <div class="gallery-container">
+                              <img
+                                src="{{ Storage::url($gallery->photos ?? '') }}"
+                                alt=""
+                                class="w-100"
+                              />
+                              <a href="{{ route('dashboard-product-gallery-delete', $gallery->id) }}" class="delete-gallery">
+                                <img src="/images/icon-delete.svg" alt="" />
+                              </a>
+                            </div>
                           </div>
-                        </div>
-
-                        <div class="col-md-4">
-                          <div class="gallery-container">
-                            <img
-                              src="/images/product-card-2.png"
-                              alt=""
-                              class="w-100"
-                            />
-                            <a href="#" class="delete-gallery">
-                              <img src="/images/icon-delete.svg" alt="" />
-                            </a>
-                          </div>
-                        </div>
-
-                        <div class="col-md-4">
-                          <div class="gallery-container">
-                            <img
-                              src="/images/product-card-3.png"
-                              alt=""
-                              class="w-100"
-                            />
-                            <a href="#" class="delete-gallery">
-                              <img src="/images/icon-delete.svg" alt="" />
-                            </a>
-                          </div>
-                        </div>
+                        @endforeach
 
                         <div class="col-12">
-                          <input
+                          <form action="{{ route('dashboard-product-gallery-upload') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <input type="hidden" name="products_id" value="{{ $product->id }}">
+                            <input
                             type="file"
+                            name="photos"
                             id="file"
                             style="display: none"
-                            multiple
-                          />
-                          <button
-                            class="btn btn-secondary btn-block mt-3"
-                            onclick="thisFileUpload()"
-                          >
-                            Add Photo
-                          </button>
+                            onchange="form.submit()"
+                            />
+                            <button
+                              type="button"
+                              class="btn btn-secondary btn-block mt-3"
+                              onclick="thisFileUpload()"
+                            >
+                              Add Photo
+                            </button>
+                          </form>
                         </div>
                       </div>
                     </div>
