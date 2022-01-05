6a036ea1f398a1a50a2dda5785047ec871f90715
heliana <helianachristianti@gmail.com>
2022-01-05 18:14:42 +0700

    selesai
    edit bagian admin transaction

--- bwastore-laravel/resources/views/layouts/admin.blade.php
+++ bwastore-laravel/resources/views/layouts/admin.blade.php
@@ -55,7 +55,7 @@
             </a>
             <a
               href="{{ route('transaction.index') }}"
-              class="list-group-item list-group-item-action"
+              class="list-group-item list-group-item-action {{ (request()->is('admin/transaction*')) ? 'active' : ''  }}"
             >
               Transactions
             </a>
--- bwastore-laravel/routes/web.php
+++ bwastore-laravel/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\ProductGalleryController;
+use App\Http\Controllers\Admin\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -156,7 +157,7 @@ Route::prefix("admin")
         Route::resource("user", UserController::class);
         Route::resource("product", ProductController::class);
         Route::resource("product-gallery", ProductGalleryController::class);
-        Route::resource("transaction", "TransactionController");
+        Route::resource("transaction", TransactionController::class);
     });
 
 Auth::routes();
